84a79150022a1f05fe6a6ad6dc1af9caf59fa199
Celsius Aray <emilioaray@gmail.com>
2022-02-11 18:54:07 -0400

    Ignere Build TypeScrip

--- next.config.js
+++ next.config.js
@@ -2,12 +2,21 @@ module.exports = (phase, { defaultConfig }) => {
   /**
    * @type {import('next').NextConfig}
   */
+  const { i18n } = require('./next-i18next.config');
   const nextConfig = {
     /* config options here */
     i18n: {
     locales: ['en', 'es'],
-    defaultLocale: 'es'
-  },
-  }
+    defaultLocale: 'en'
+    },
+     typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+    },
+    productionBrowserSourceMaps: true,
+    }
   return nextConfig;
 }
