2e1a05c48e791547a020ff5d9d8ee1198015a87e
Celsius Aray <emilioaray@gmail.com>
2022-02-17 18:56:50 -0400

    Ifswitch Partial

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,34 +1,140 @@
 import { useTranslation } from 'next-i18next'
-
+import Image from 'next/image'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
 export default function Projects() {
   const { t } = useTranslation('common')
 
   return (
     <>
       <section className="container">
-        <div id="Projects" className="max__container container_Margen">
-          <div className="Project_card">
-            <h2 className="Project_h2">{t('projectsT')}</h2>
+        <div id="Projects" className="max__container ">
+          <div className="container_Margen">
+            <div className="Project_card">
+              <h2 className="Project_h2">{t('projectsT')}</h2>
 
-            <div className="Project_subCardGrid">
-              <div className="Project_subCard">
-                <h3>P1</h3>
-                <div className="pictureGrid"></div>
-              </div>
+              <div className="Project_subCardGrid">
+                <Carousel
+                  centerMode={false}
+                  swipeable={true}
+                  emulateTouch={true}
+                  infiniteLoop={false}
+                  showIndicators={false}
+                  showStatus={true}
+                >
+                  <div className="Project_subCard">
+                    <h3>P1</h3>
+                  </div>
 
-              <div className="Project_subCard">
-                <h3>P2</h3>
-                <div className="pictureGrid"></div>
-              </div>
+                  <div className="Project_subCard">
+                    <h3>P2</h3>
+                  </div>
 
-              <div className="Project_subCard">
-                <h3>P3</h3>
-                <div className="pictureGrid"></div>
+                  <div className="Project_subCard">
+                    <h3>Ifswitch</h3>
+
+                    <Image
+                      src="/assets/img/projects/www.ifswitch.com.webp"
+                      alt="Webpage ifswitch.com"
+                      width={375}
+                      height={765}
+                      layout="responsive"
+                    />
+                  </div>
+
+                  <div className="Project_subCard">
+                    <h3>Ifswitch</h3>
+                    <picture>
+                      <Image
+                        src="/assets/img/projects/www.ifswitch.com.webp"
+                        alt="Webpage ifswitch.com"
+                        width={375}
+                        height={765}
+                        layout="responsive"
+                      />
+                    </picture>
+                  </div>
+
+                  <div className="Project_subCard">
+                    <h3>Ifswitch</h3>
+                    <picture>
+                      <Image
+                        src="/assets/img/projects/www.ifswitch.com.webp"
+                        alt="Webpage ifswitch.com"
+                        width={375}
+                        height={765}
+                        layout="responsive"
+                      />
+                    </picture>
+                  </div>
+
+                  <div className="Project_subCard">
+                    <h3>Ifswitch</h3>
+                    <picture>
+                      <Image
+                        src="/assets/img/projects/www.ifswitch.com.webp"
+                        alt="Webpage ifswitch.com"
+                        width={375}
+                        height={765}
+                        layout="responsive"
+                      />
+                    </picture>
+                  </div>
+                </Carousel>
               </div>
             </div>
           </div>
         </div>
       </section>
+      <style jsx global>{`
+        .swipe-container {
+          display: flex;
+          overflow: auto;
+          overflow-x: scroll;
+          scroll-snap-type: x mandatory;
+        }
+        /* scrollbar should be hidden */
+        .swipe-container::-webkit-scrollbar {
+          display: none;
+        }
+        /* main element should always snap into view */
+        .swipe-element {
+          scroll-snap-align: start;
+          font-family: sans-serif;
+          padding: 16px;
+          background-color: whitesmoke;
+        }
+        .carousel-root {
+          display: grid;
+          outline: none;
+        }
+        .carousel .slider-wrapper.axis-horizontal .slider .slide {
+          /*
+          min-width: fit-content !important;
+          */
+        }
+        .carousel .control-arrow,
+        .carousel.carousel-slider .control-arrow {
+          opacity: 1;
+          margin-right: -3px;
+          -webkit-backdrop-filter: blur(5px);
+          backdrop-filter: blur(5px);
+          background: #5479ff61;
+          width: 2.5rem;
+        }
+
+        button[aria-label='previous slide / item'] {
+          border-radius: 0px 15px 15px 0px;
+        }
+
+        .slider-wrapper,
+        button[aria-label='next slide / item'] {
+          border-radius: 15px 0px 0px 15px;
+        }
+        .carousel .carousel-status {
+          z-index: 2;
+        }
+      `}</style>
       <style jsx>{`
         .container {
           display: grid;
@@ -54,27 +160,19 @@ export default function Projects() {
         .container_Margen {
           display: grid;
           align-items: center;
-          margin-top: var(--container_topMargin);
-          /*
-          margin-top: var(--container_topMargin);
-          padding-bottom: calc(var(--container_topMargin) * 1.5);
-          */
+          max-width: var(--max-widthContainer);
         }
 
         .Project_card {
           border-radius: 1rem;
-          padding: 4% 6%;
+          padding: 4% 0%;
           background: var(--colorCard2Background);
+          /* max-width: var(--max-widthContainer); */
           height: fit-content;
-          /* 
-          margin-top: var(--container_topMargin);
-          */
           background-image:
             radial-gradient(circle at 17% 83%, var(--colorB3Gradient) 0%, transparent 74%) ,
             radial-gradient(circle at 60% 23%, var(--colorB1Gradient) 0%, transparent 120%);
         }
-
-       
         .Project_h2 {
           font-size: 3rem;
           font-weight: 300;
@@ -92,20 +190,25 @@ export default function Projects() {
           text-align: center;
           min-height: 3rem;
           color: var(--colorWhite);
+          user-select: none;
         }
         .Project_subCardGrid {
+        /* 
           display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(13rem, 1fr));
+          grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
           gap: 1rem;
+        */
         }
 
         .Project_subCard {
           border-radius: 1rem;
           padding: 8% 6%;
-          min-height: 17rem;
+          height: 100%;
           -webkit-backdrop-filter: blur(5px);
           backdrop-filter: blur(5px);
-          /* background: var(--backgroundSubCard); */
+          margin: 0 0.5rem; 
+          width: 15rem;
+          margin-left: 1.4rem;
         }
 
          .Project_subCard:nth-child(2n) {
@@ -116,42 +219,18 @@ export default function Projects() {
           background: var(--backgroundSubCardInpar);
         }
 
-        .pictureGrid {
-          display: grid;
-          gap: 1rem;
-          grid-template-columns: repeat(auto-fit, minmax(4rem, 1fr));
-          align-items: center;
-          justify-items: center;
-          margin-top: 1rem;
+
+       picture {
+          display: block;
+          width: 100%;
+          user-select: none;
+          border-radius: 1rem;
+          overflow: hidden;
         }
 
-        .pictureCenter {
-          grid-column-start: 1;
-          grid-column-end: 4;
-        }
 
-        picture {
-          display: grid;
-          height: 100%;
-          align-items: center;
-        }
-        p {
-          font-size: 1.2rem;
-          margin-block-end: 1em;
-        }
 
         @media (max-width: 834px) {
-/*
-          .container {
-            background: repeating-linear-gradient(
-              45deg,
-              #7c7c7c15 0px,
-              #7c7c7c15 3px,
-              rgba(255, 255, 255, 0) 3px,
-              rgba(255, 255, 255, 0) 7.04%
-            );
-          }
-*/
           .Project_card {
             background-image:
               radial-gradient(circle at 50% 25%, var(--colorB3Gradient) 0%, transparent 74%),
@@ -162,7 +241,7 @@ export default function Projects() {
           }
 
           .container_Margen {
-            margin-top: calc(var(--container_topMargin) + 2rem);
+
           }
         }
         @media (min-width: 834px) and (max-width: 1280px) {
@@ -175,9 +254,7 @@ export default function Projects() {
               rgba(255, 255, 255, 0) 7vw
             );
           }
- 
-          
-        
+
       `}</style>
     </>
   )
